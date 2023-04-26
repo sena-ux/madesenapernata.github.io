89314bc6c2b1019e98df7052b0c952cc13308b8a
sena-ux <senapenata010704@gmail.com>
2023-04-26 11:32:22 +0800

    a

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,8 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()]
-// })
-
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/madesenapernata.github.io/',
-  build: {
-    outDir: 'build',
-    assetsDir: 'assets',
-    assetsInlineLimit: 4096,
-    sourcemap: false,
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, '/src/main.jsx'),
-    },
-  },
-});
-
+  base: '/madesenapernata.github.io'
+})
